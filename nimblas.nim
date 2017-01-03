b1d4b864b774cf6f768b6d565c9f5726340c30cb
Andrea Ferretti <ferrettiandrea@gmail.com>
2017-01-03 18:35:56 +0100

    Fixed package structure

--- nimblas.nim
+++ nimblas.nim
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include private/common
+include nimblas/private/common
 
 type
   TransposeType* = enum
